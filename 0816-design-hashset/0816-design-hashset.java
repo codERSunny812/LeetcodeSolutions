684c3e6f1df52450a2ece70544d48974888243c3
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2025-01-11 22:14:29 +0530

    Time: 292 ms (13.13%), Space: 49.3 MB (86.93%) - LeetHub

---
+++ 0816-design-hashset/0816-design-hashset.java
@@ -0,0 +1,66 @@
+class Node{
+    Node next;
+    int data;
+    public Node(int key){
+        this.data=key;
+        this.next=null;
+    }
+}
+class MyHashSet {
+    Node head;
+    public MyHashSet() {
+       head=null; 
+    }
+    
+    public void add(int key) {
+        if(head==null) 
+        {
+            head= new Node(key);
+            return;
+        }
+        boolean doesExists=contains(key);
+        if(!doesExists){
+            Node temp=head;
+            while(temp.next!=null)
+                temp=temp.next;
+            
+            temp.next=new Node(key);
+        }
+    }
+    
+    public void remove(int key) {
+        if(head==null) return;
+        if(head.data==key){
+            head=head.next;
+            return;
+        }
+        
+        Node temp=head;
+        while(temp.next!=null){
+            if(temp.next.data==key)
+                temp.next=temp.next.next;
+            else
+                temp=temp.next;
+        }
+    }
+    
+    public boolean contains(int key) {
+        if(head==null) return false;
+        if(head.data==key) return true;
+        
+        Node temp=head;
+        while(temp.next!=null){
+            if(temp.next.data==key) return true;
+            temp=temp.next;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
