8f647df83bacde68fe6fdac3aba862776df1aa27
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2025-06-20 06:18:17 +0530

    Time: 0 ms (100%), Space: 56.8 MB (75.62%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -4,6 +4,8 @@
  */
 var removeDuplicates = function(nums) {
     
+    // handle the corner case
+    if(nums.length < 0) return 0;
     let i = 0; // one pointer to the starting of the array
 
     for(let j = 0; j< nums.length;j++){
