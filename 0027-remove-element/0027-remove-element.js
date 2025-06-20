40b9cdd114ed95017aa507bc660cec5b8034b15e
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2025-06-20 06:59:11 +0530

    Time: 0 ms (100%), Space: 52.6 MB (95.08%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -5,10 +5,13 @@
  */
 var removeElement = function(nums, val) {
 
+// handle the corner case
 if(nums.length < 0) return 0;
 
+// pointer at the index 0 of the array
  let i =0;
  for(let j=0;j<nums.length;j++){
+    // only swap if the value is not equal to the val 
     if(nums[j] != val){
         nums[i] = nums[j];
         i=i+1;
