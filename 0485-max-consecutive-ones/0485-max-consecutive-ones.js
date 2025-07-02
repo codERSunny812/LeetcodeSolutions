0eb45823eba6076eff5cb4732118fe968f9fdd66
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2025-07-02 05:50:45 +0530

    Time: 2 ms (64.22%), Space: 59.6 MB (66.75%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let count = 0;
+    let maxCount = 0;
+
+    for(let i = 0;i< nums.length;i++){
+        //checking the nums element 
+
+        if(nums[i] != 0){
+            count++;
+        }else{
+        count = 0;
+        }
+
+        if(count > maxCount){
+            maxCount = count;
+        }
+    }
+
+    return maxCount;
+};
