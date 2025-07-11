fd7af98730175b822fd53eb2ede54350fc7f561c
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2025-07-12 00:21:37 +0530

    Time: 52 ms (6.66%), Space: 53.2 MB (68.39%) - LeetHub

--- 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -7,13 +7,14 @@ var expect = function(val) {
     return {
         toBe:function(val1){
 
-            if(val1 != val ) throw new Error("Not Equal")
+            if(val1 !== val ) throw new Error("Not Equal")
             else return true;
 
         },
         notToBe: function (val2){
 
-            if(val2 == val) throw new Error("Equal") else return true;
+            if(val2 === val) throw new Error("Equal");
+            else return true;
 
         }
     }
