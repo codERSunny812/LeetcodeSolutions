0675b338124f40e61131b10e661c93e75263ff3c
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2025-07-19 11:44:42 +0530

    Time: 59 ms (21.14%), Space: 59 MB (66.95%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -10,30 +10,52 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function(head) {
-    
-    let nodesSet = new Set();
-    let curr = head;
+var hasCycle = function (head) {
+
+    // let nodesSet = new Set();
+    // let curr = head;
 
 
-    //base case 
-    if(!head) return false
+    // //base case 
+    // if(!head) return false
 
-    while(curr){
+    // while(curr){
 
-         if(nodesSet.has(curr)){
-            return true;
-         }else{
-            nodesSet.add(curr);
-            curr=curr.next;
-         }
+    //      if(nodesSet.has(curr)){
+    //         return true;
+    //      }else{
+    //         nodesSet.add(curr);
+    //         curr=curr.next;
+    //      }
 
 
 
+
+
+    // }
+
+
+    // return false;
+
+    // approach - 2
 
    
+
+    if(head == null || head.next == null) return false;
+
+     let slow = head;
+    let fast = head.next;
+
+
+    while(slow != fast){
+
+
+       if(fast == null || fast.next == null) return false;
+
+        slow = slow.next;
+        fast=fast.next.next
     }
 
 
-    return false;
+    return true;
 };
