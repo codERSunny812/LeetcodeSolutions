3b96a6e2eef8ca87d2e994eafed42c71aad2095c
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2025-07-20 11:19:46 +0530

    Time: 4 ms (76.33%), Space: 76.2 MB (70.51%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -11,36 +11,97 @@
  */
 var isPalindrome = function(head) {
 
-    if(head == null) return false;
+    // approach -1 
 
-    if(head.next == null ) return true
+    // if(head == null) return false;
 
-    let curr = head;
-    const arr = [];
+    // if(head.next == null ) return true
+
+    // let curr = head;
+    // const arr = [];
 
 
-    while(curr != null){
-        arr.push(curr.val);
-        curr = curr.next;
-    }
+    // while(curr != null){
+    //     arr.push(curr.val);
+    //     curr = curr.next;
+    // }
 
-    //checking the array 
-    let i=0;
-    let j = arr.length -1;
+    // //checking the array 
+    // let i=0;
+    // let j = arr.length -1;
 
-    while(i<j){
+    // while(i<j){
 
-        if(arr[i] != arr[j]){
-            return false;
-        }
+    //     if(arr[i] != arr[j]){
+    //         return false;
+    //     }
 
-        i++;
-        j--;
+    //     i++;
+    //     j--;
     
 
-    }
+    // }
 
-    return true;
+    // return true;
+
+
+
+    // approach -2 
+
+ // corner cases 
+
+ if(!head && !head.next) return false;
+
+//  find the mid of the LL 
+let length = 0;
+ let curr = head;
+
+ while(curr != null){
+    length++;
+    curr = curr.next;
+ }
+
+ const midOfLL = Math.floor(length/2);
+
+ let mid=head;
+
+
+  for(let i=0;i<midOfLL;i++){
+    mid = mid.next;
+  }
+
+
+//   reverse the linked list from the mid 
+
+let prev = null;
+let curr2 = mid;
+
+
+while(curr2 != null){
+    let temp = curr2.next;
+    curr2.next = prev;
+    prev = curr2;
+    curr2= temp;
+}
+
+// check for palindrome 
+let firstNode = head;
+let endNode = prev;
+
+while(endNode != null){
+    if(firstNode.val != endNode.val) return false;
+
+    firstNode = firstNode.next;
+    endNode = endNode.next;
+}
+
+
+return true;
+
+
+
+
+
 
 
 
