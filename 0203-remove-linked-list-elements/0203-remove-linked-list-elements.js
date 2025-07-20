05f04f2a5deb4e9b89ba47418f776a1f59504339
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2025-07-20 14:37:58 +0530

    Time: 0 ms (100%), Space: 60.2 MB (89.99%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+
+    if(!head) return null;
+
+    if(head.val == val) {
+        head = head.next;
+    }
+
+    let curr = head;
+    let prev =null;
+
+    while(curr != null){
+
+        if(curr == head && curr.val == val){
+            head= head.next;
+            curr = head;
+            
+        } else if(curr.val == val){
+        let temp = curr.next;
+            prev.next = curr.next;
+            curr= temp;
+        }else{
+            prev = curr;
+            curr=curr.next;
+        }
+    }
+    return head;
+};
