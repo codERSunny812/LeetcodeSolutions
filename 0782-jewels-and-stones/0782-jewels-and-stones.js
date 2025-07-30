f3e1f6d72f7b28aea0824acbb70c8d417b7de97e
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2025-07-30 17:13:06 +0530

    Time: 0 ms (100%), Space: 52.6 MB (97.18%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.js
+++ 0782-jewels-and-stones/0782-jewels-and-stones.js
@@ -6,10 +6,28 @@
 var numJewelsInStones = function(jewels, stones) {
     let stoneCount = 0;
 
-    for(let i=0;i<stones.length;i++){
-        console.log(stones[i])
-        if(jewels.includes(stones[i])){
-            stoneCount++;
+    // approach -1
+
+    // for(let i=0;i<stones.length;i++){
+    //     console.log(stones[i])
+    //     if(jewels.includes(stones[i])){
+    //         stoneCount++;
+    //     }
+    // }
+
+
+    // approach - 2
+
+    let jSet = new Set();
+
+    for(let i=0;i<jewels.length;i++){
+        jSet.add(jewels[i]);
+    }
+
+
+      for(let i=0;i<stones.length;i++){
+        if(jSet.has(stones[i])){
+        stoneCount++;
         }
     }
 
