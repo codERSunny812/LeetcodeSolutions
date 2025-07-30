62a92d5d0c48f0f9c0b0d4184bcb6f24ab72ad10
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2025-07-30 06:52:04 +0530

    Time: 2 ms (9.51%), Space: 53 MB (87.15%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,7 +3,34 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-
-    return s.trim().split(" ").pop().length;
     
+
+    let length = s.length-1;
+    console.log('s length',length)
+
+    //remove the last space 
+
+    while(length >= 0){
+        if(s[length] == " "){
+            --length;
+        }else{
+            break;
+        }
+    }
+
+
+    // new length after the end space is removed
+    let newLen = s.length-1;
+    console.log("new string length after end space is removeed",newLen)
+    let strLen = 0;
+
+    for(let i=length;i>=0;i--){
+        if(s[i] != " " ){
+            strLen++;
+        }else{
+            break;
+        }
+    }
+
+    return strLen;
 };
