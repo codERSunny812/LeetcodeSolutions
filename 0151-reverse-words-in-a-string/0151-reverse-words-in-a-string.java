ccbdf2dc8b12d7614e898606b5809bdbda1b2657
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2026-01-29 22:21:01 +0530

    Time: 5 ms (87.51%), Space: 45 MB (28.16%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,25 +1,25 @@
 class Solution {
     public String reverseWords(String s) {
-        // Split the string by spaces into an array
-        String[] arr = s.split(" ");
+        s = s.trim();
 
-        // Create a StringBuilder to store the reversed words
-        StringBuilder reversed = new StringBuilder();
-
-        // Iterate over the words in reverse order and append them to the StringBuilder
-        for (int i = arr.length - 1; i >= 0; i--) {
-            String word = arr[i].trim(); //remove the extra spaces 
-            if (!word.isEmpty()) {
-               if (reversed.length() > 0) {
-                    reversed.append(" "); // Add a space between words
-                }
-              
-                reversed.append(word); // Append the word
-                
-            }
+        String [] arr = s.split(" "); // ["the","sky","is","blue"]
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = arr.length-1; i>=0 ; i--){
             
-        }
-
-        return reversed.toString(); // Convert the StringBuilder to a String and return
+            if(!arr[i].isEmpty()){
+            
+               sb.append(arr[i].trim());
+               if(i>0){
+                sb.append(" ");
+               }
+               
+            }
+          
+        }        
+        
+        return sb.toString();
+        
     }
 }
+
