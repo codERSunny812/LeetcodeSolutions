2affa848d27cf5a965b977191caea47ea069a03a
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2026-02-01 19:38:41 +0530

    Time: 13 ms (19.9%), Space: 47.1 MB (40%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,26 +1,33 @@
 class Solution {
     public int singleNumber(int[] nums) {
-    //  HashMap<Integer,Integer> map  = new HashMap<>();
-    //  for(int i = 0 ; i < nums.length ; i++){
-    //     if(map.containsKey(nums[i])){
-    //         map.put(nums[i],map.getValue(nums[i])++);
-    //     }
-    //     else{
-    //         map.put(nums[i],1);
-    //     }
+     HashMap<Integer,Integer> map  = new HashMap<>();
+     int ans = 0 ;
+     for(int i = 0 ; i < nums.length ; i++){
+        if(map.containsKey(nums[i])){
+            map.put(nums[i],map.get(nums[i])+1);
+        }
+        else{
+            map.put(nums[i],1);
+        }
  
-    //  }
-    //  return map.getKey(1);
+     }
+     for(Map.Entry<Integer,Integer> e: map.entrySet()){
+        if(e.getValue()==1){
+            ans = e.getKey();
+        }
+     }
+     
+     return ans;
+     }
+}   
     
-    
-int ans = 0;
-for(int i=0;i<nums.length;i++){
-    ans = ans ^ nums[i];
-    System.out.println(ans);
-}
+// int ans = 0;
+// for(int i=0;i<nums.length;i++){
+//     ans = ans ^ nums[i];
+//     System.out.println(ans);
+// }
 
-return ans;
-    }
-}
+// return ans;
+
 
 
