e8209c051fda8075674d83207adeff2c8415b7cf
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2026-04-08 17:36:32 +0530

    Time: 2 ms (99.28%), Space: 47.2 MB (29.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,31 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
     
-        for(int i =0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j] == target){
-                   return new int[] {i,j};
-                }
-            }
+        // approach - 1 
+//         for(int i =0;i<nums.length;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//                 if(nums[i]+nums[j] == target){
+//                    return new int[] {i,j};
+//                 }
+//             }
             
-        }
-return  new int[0];
+//         }
+// return  new int[0];
+
+
+
+// approach 2 
+HashMap<Integer,Integer> mpp = new HashMap<>();
+for(int i=0;i<nums.length;i++){
+    int diff = target - nums[i];
+
+    if(mpp.containsKey(diff)){
+        return new int[] { mpp.get(diff),i };
+    }
+
+    mpp.put(nums[i],i);
+}
+
+return new int[] {};
     }
 }
