7d8342a920ffc25783ea7e62cfb57ba0e94663c2
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2026-07-07 19:14:17 +0530

    Time: 0 ms (100%), Space: 43.8 MB (10.68%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int low = 0, mid = 0 , high = nums.length -1 ;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums,mid,low);
+                mid++;
+                low++;
+
+            }else if (nums[mid] == 2){
+             swap(nums,mid,high);
+        
+             high--;
+            }else{
+                mid++;
+            }
+        }
+
+   
+
+
+
+        
+    }
+
+      public void swap(int []arr, int i, int j){
+        int temp = arr[i];
+        arr[i]= arr[j];
+        arr[j] = temp;
+     }
+}
