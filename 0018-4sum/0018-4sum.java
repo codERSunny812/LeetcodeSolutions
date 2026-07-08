ec61df90cf5551465f9bee3f95dbc8b0b6b0e847
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2026-07-08 17:20:20 +0530

    Time: 21 ms (40.01%), Space: 45.9 MB (31.66%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
 
-//sort the arrya 
+        //sort the arrya 
         Arrays.sort(nums);
 
         //creating an array list
@@ -9,42 +9,43 @@ class Solution {
 
         int n = nums.length;
 
-        for(int i=0;i<n-3;i++){
-             //skip the dublicate element
-            if(i> 0 && nums[i] == nums[i-1]) continue;
+        for (int i = 0; i < n - 3; i++) {
+            //skip the dublicate element
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
 
-            for (int j=i+1;j<n-2;j++){
-            
-              if(j> i+1 && nums[j] == nums[j-1]) continue;
+            for (int j = i + 1; j < n - 2; j++) {
 
-            int left = j+1;
-            int right = n-1;
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
 
+                int left = j + 1;
+                int right = n - 1;
 
-            while(left < right){
-                long sum = (long) nums[left]+nums[right]+nums[i]+nums[j];
+                while (left < right) {
+                    long sum = (long) nums[left] + nums[right] + nums[i] + nums[j];
 
-                if(sum == target){
-                    //added in the list
-                    lpp.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
-                    left++;
-                    right--;
-
-                    //check for dublicate 
-
-                    while(left < right && nums[left] == nums[left-1]){
+                    if (sum == target) {
+                        //added in the list
+                        lpp.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
                         left++;
-                    }
+                        right--;
 
-                    while(left < right  && nums[right] == nums[right+1]){
+                        //check for dublicate 
+
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    } else if (sum < target) {
+                        left++;
+                    } else {
                         right--;
                     }
-                }else if(sum < target){
-                    left++;
-                }else{
-                    right--;
                 }
-            }
             }
         }
 
